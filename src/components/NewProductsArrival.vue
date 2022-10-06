9278bd4db2eabc0ac4d80371daefb581a14fae54
GianlucaFabrizio <gianlucafabrizio@hotmil.com>
2022-10-06 12:30:20 +0200

    MAIN: Creazione area nuovi prodotti in arrivo

--- src/components/NewProductsArrival.vue
+++ src/components/NewProductsArrival.vue
@@ -6,28 +6,34 @@
     </div>
     <div class="contenitorCards">
       <div class="card">
-        <img src="../assets/images/product-9-500x500.jpg" alt="Bad">
-        <p>Bad (1)</p>
+        <img src="../assets/images/product-8-500x500.jpg" alt="Bad">
+        <h3>Colored pet bed</h3>
+        <h4>$18.00 - $26.00</h4>
       </div>
       <div class="card">
-        <img src="../assets/images/product-3-500x500.jpg" alt="Food">
-        <p>Food (6)</p>
+        <img src="../assets/images/product-2-500x500.jpg" alt="Food">
+        <h3>Colorful ball set</h3>
+        <h4>$29.00</h4>
       </div>
       <div class="card">
-        <img src="../assets/images/product-2-500x500.jpg" alt="Toys">
-        <p>Toys (6)</p>
-      </div>
-      <div class="card">
-        <img src="../assets/images/product-4-500x500.jpg" alt="Transport">
-        <p>Transport (6)</p>
-      </div>
-      <div class="card">
-        <img src="../assets/images/product-4-500x500.jpg" alt="Transport">
-        <p>Transport (6)</p>
+        <img src="../assets/images/product-3-500x500.jpg" alt="Toys">
+        <h3>Dog bone</h3>
+        <h4><span>$29.00</span>$18.00</h4>
       </div>
       <div class="card">
         <img src="../assets/images/product-4-500x500.jpg" alt="Transport">
-        <p>Transport (6)</p>
+        <h3>Animal transport bag</h3>
+        <h4>$29.00</h4>
+      </div>
+      <div class="card">
+        <img src="../assets/images/product-5-500x500.jpg" alt="Transport">
+        <h3>Animal transport cage</h3>
+        <h4>$35.00</h4>
+      </div>
+      <div class="card">
+        <img src="../assets/images/product-10-500x500.jpg" alt="Transport">
+        <h3>Closable cat litter</h3>
+        <h4>$16.00</h4>
       </div>
     </div>
   </div>
@@ -49,7 +55,7 @@ export default {
 
   .text {
     width: 50%;
-    margin-bottom: 50px;
+    margin-bottom: 15px;
 
     h2 {
       font-size: 1.7rem;
@@ -66,19 +72,30 @@ export default {
     flex-wrap: wrap;
     justify-content: center;
     width: 100%;
+    margin-bottom: 40px;
 
     .card {
-      margin: 5px;
-      width: calc(100vh / 3 - 10px + 10%);
-      height: calc(100vh / 3 - 10px + 10%);
+      margin: 18px 5px;
+      width: calc(100vh / 3 - 10px);
 
       img {
         width: 100%;
-        height: 100%;
       }
 
-      p{
-        margin: 20px 0;
+      h3{
+        font-size: 0.9rem;
+        padding: 8px 0;
+      }
+
+      h4{
+        color: rgb(136, 136, 136);
+        font-size: 0.75rem;
+
+        span{
+          font-size: 0.6rem;
+          padding: 10px;
+          text-decoration: line-through #000;
+        }
       }
     }
   }
