a52bdd600b4bc7c9bddf261d0075814cd903b9d8
GianlucaFabrizio <gianlucafabrizio@hotmil.com>
2022-10-06 09:44:07 +0200

    MAIN: Creazione area Prodotti in vendita

--- src/components/NewProductsArrival.vue
+++ src/components/NewProductsArrival.vue
@@ -1,15 +1,86 @@
 <template>
-  <div>
-
+  <div class="container">
+    <div class="text">
+      <h2>New products arrival</h2>
+      <p>Latest products</p>
+    </div>
+    <div class="contenitorCards">
+      <div class="card">
+        <img src="../assets/images/product-9-500x500.jpg" alt="Bad">
+        <p>Bad (1)</p>
+      </div>
+      <div class="card">
+        <img src="../assets/images/product-3-500x500.jpg" alt="Food">
+        <p>Food (6)</p>
+      </div>
+      <div class="card">
+        <img src="../assets/images/product-2-500x500.jpg" alt="Toys">
+        <p>Toys (6)</p>
+      </div>
+      <div class="card">
+        <img src="../assets/images/product-4-500x500.jpg" alt="Transport">
+        <p>Transport (6)</p>
+      </div>
+      <div class="card">
+        <img src="../assets/images/product-4-500x500.jpg" alt="Transport">
+        <p>Transport (6)</p>
+      </div>
+      <div class="card">
+        <img src="../assets/images/product-4-500x500.jpg" alt="Transport">
+        <p>Transport (6)</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-name: 'NewProductsArrival'
+  name: 'NewProductsArrival'
 }
 </script>
 
 <style lang="scss" scoped>
+.container {
+  padding: 80px 10% 50px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
 
+  .text {
+    width: 50%;
+    margin-bottom: 50px;
+
+    h2 {
+      font-size: 1.7rem;
+    }
+
+    p {
+      line-height: 1.5rem;
+      padding: 15px 0;
+    }
+  }
+
+  .contenitorCards {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    width: 100%;
+
+    .card {
+      margin: 5px;
+      width: calc(100vh / 3 - 10px + 10%);
+      height: calc(100vh / 3 - 10px + 10%);
+
+      img {
+        width: 100%;
+        height: 100%;
+      }
+
+      p{
+        margin: 20px 0;
+      }
+    }
+  }
+}
 </style>
--- src/components/ProductsForSale.vue
+++ src/components/ProductsForSale.vue
@@ -1,15 +1,100 @@
 <template>
-  <div>
-
+  <div class="container">
+    <div class="areaTopImages">
+      <div class="text">
+        <h4>All-time best sellers</h4>
+        <h2>Items everyone loves</h2>
+      </div>
+      <input type="button" value="View all products">
+    </div>
+    <div class="cards">
+      <div class="card">
+        <img src="../assets/images/product-21-400x400.jpg" alt="Transport cage">
+        <h3>Transport cage</h3>
+        <h4>$25.00</h4>
+      </div>
+      <div class="card">
+        <img src="../assets/images/product-20-400x400.jpg" alt="Dog leash">
+        <h3>Dog leash</h3>
+        <h4>$25.00</h4>
+      </div>
+      <div class="card">
+        <img src="../assets/images/product-16-400x400.jpg" alt="Animal transport cage">
+        <h3>Animal transport cage</h3>
+        <h4><span>$35.00</span>$25.00</h4>
+      </div>
+      <div class="card">
+        <img src="../assets/images/product-11-400x400.jpg" alt="Colorful cat leash">
+        <h3>Colorful cat leash</h3>
+        <h4>$12.00</h4>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-    name: 'ProductsForSale'
+  name: 'ProductsForSale'
 }
 </script>
 
 <style lang="scss" scoped>
+.container {
+  margin: 30px 10%;
 
+  .areaTopImages {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 35px;
+
+    .text {
+      h4 {
+        font-size: 0.8rem;
+      }
+
+      h2 {
+        margin: 20px 0;
+      }
+    }
+
+    input {
+      padding: 10px 20px;
+      border-radius: 25px;
+      border: none;
+      color: #fff;
+      background-color: #3d6f42;
+    }
+  }
+
+  .cards{
+    display: flex;
+    justify-content: space-between;
+
+    .card{
+      width: calc(100% / 4 - 20px);
+      text-align: center;
+
+      img{
+        width: 100%;
+      }
+
+      h3{
+        font-size: 0.9rem;
+        padding: 10px;
+      }
+
+      h4{
+        font-size: 0.85rem;
+        color: rgb(153, 153, 153);
+
+        span{
+          font-size: 0.7rem;
+          padding: 10px;
+          text-decoration: line-through #000;
+        }
+      }
+    }
+  }
+}
 </style>
